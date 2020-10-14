3b3f12a512a2120f11fe94c2124e61461711b904
Choton <choton654@gmail.com>
2020-10-14 19:32:02 +0530

    ...

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://ecommerce-nextjs-theta.vercel.app/"
+    ? "https://ecommerce-nextjs-theta.vercel.app"
     : "http://localhost:3000";
 
 export default baseUrl;
